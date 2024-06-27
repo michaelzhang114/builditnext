876db32d7bc8f1340597cd07b27c93e0c8a3ce0a
michaelzhang114 <michaelzhang6284@gmail.com>
2024-06-26 18:13:00 -0700

    changed env

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 						<h1 className="text-5xl font-bold">Hello there</h1>
 						<p className="py-6">
 							Providenata cupiditate voluptatem et in. Quaerat
-							fugiat ut assumenda excepturi exercitationem quasi.
+							fugiat ut aassumenda excepturi exercitationem quasi.
 							In deleniti eaque aut repudiandae et a id nisi.
 						</p>
 						<button className="btn btn-primary">Get Started</button>
